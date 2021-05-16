8036c6c17e922e0960b070fa426ce8f0e9cc55b3
Carlo Tomaso Cantarella <cantarella.carlotomaso@gmail.com>
2021-05-16 11:47:04 +0200

    prima impostazione componente Header

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import styles from '../style/Header.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBookReader, faPlusSquare, faPlus } from '@fortawesome/free-solid-svg-icons'
+
+const Header = () => {
+    return (
+        <div className={styles.container}>
+            <FontAwesomeIcon size='3x' icon={faBookReader} />
+            <div className={styles.rightContainer}>
+                <p style={{ marginRight: '20px' }}>I miei libri</p>
+                <FontAwesomeIcon size='3x' icon={faPlusSquare} />
+            </div>
+
+        </div>
+    )
+}
+
+export default Header
