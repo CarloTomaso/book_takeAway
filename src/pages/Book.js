a43bbb3b4ca2a1dd670324714321260034bb393a
Carlo Tomaso Cantarella <cantarella.carlotomaso@gmail.com>
2021-05-18 22:58:43 +0200

    creazione pagina book

--- src/pages/Book.js
+++ src/pages/Book.js
@@ -1,7 +1,33 @@
-const Book = () => {
-    return (
-        <div className="App">
-            <h1>Pagina Book</h1>
+import { useState, useEffect } from 'react';
+import styles from '../style/Book.module.css'
+import axios from 'axios';
+
+const Book = (props) => {
+    const [bookData, setBookData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(async () => {
+        const bookId = props.match.params.id;
+        const bookData = await axios.get(`https://www.googleapis.com/books/v1/volumes/${bookId}`);
+        await setBookData(bookData.data.volumeInfo)
+        setLoading(false)
+    }, [])
+
+    return loading ? <p> Carico </p> : (
+        <div className={styles.container}>
+            <div className={styles.infoContainer}>
+                <div>
+                    <h2>{bookData.title}</h2>
+                    <p>{bookData.description}</p>
+                    <h4>Autore: {bookData.authors[0]}</h4>
+                </div>
+
+
+                <img className={styles.image} src={bookData.imageLinks.thumbnail} alt="" />
+            </div>
+            <div className={styles.chapterContainer}>
+
+            </div>
+
         </div>
     );
 }
